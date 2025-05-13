8fc06f3801768859ad0f3056ee5153fd013e8d30
Richard Allen <richard.t.allen@outlook.com>
2025-05-13 20:04:42 +0000

    striped back learning pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,23 @@
 import streamlit as st
 
-st.title("🎈 My new streamlit app for Richard Updated two")
+st.title("Quick Financial Data")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Some quick curves you might need"
 )
 
 
 
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+#import pandas as pd
+#df = pd.DataFrame({
+#  'first column': [1, 2, 3, 4],
+#  'second column': [10, 20, 30, 40]
+#})
+
+
+
+
+
 
 # streamlit assumes the df should be writen to screen so allows you to skip
 # the write command - magic
-df
+#df
